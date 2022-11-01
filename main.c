3e1120caf8a07542b93f72bc8b589dce2cdd752c
dnjyj1024 <dnjyj1024@naver.com>
2022-11-01 16:53:53 +0900

    03_2

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 int main(void)
 {
 int i;
-int grade[SIZE];
-int score[SIZE];
-for (i=0; i<SIZE; i++)
-grade[i] = rand() % 100;
-
-for (i=0; i<SIZE; i++)
-score[i] = grade[i];
-
-for( i=0; i<SIZE; i++)
-printf("score[%d] = %d (%d)\n", i, score[i],grade[i]);
+int a[SIZE] = {1, 2, 3, 4, 5};
+int b[SIZE] = {1, 2, 3, 4, 5};
+int flag_same = 0;
+for (i=0; i<SIZE; i++){
+	if(a[i]!=b[i]){
+	flag_same = 1;	
+	printf("a is not the same with b\n ");
+	break;	
+	}
+}
+if(flag_same == 0)
+printf("a is the same with b.\n");
 return 0;
 }
